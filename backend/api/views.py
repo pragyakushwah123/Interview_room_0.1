620afd016150d445ffa73a64f214959775f8407d
pragyakushwah123 <114136085+pragyakushwah123@users.noreply.github.com>
2024-05-21 16:14:24 +0530

    changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,7 +17,7 @@ from django.views.decorators.csrf import csrf_exempt
 import openai
 
 SALT = "8b4f6b2cc1868d75ef79e5cfb8779c11b6a374bf0fce05b485581bf4e1e25b96c8c2855015de8449"
-URL = "interviewrdj.vercel.app"
+URL = "https://interview-room-0-1.vercel.app"
 # URL = 'http://localhost:3000'
 def mail_template(content, button_url, button_text):
     return f"""<!DOCTYPE html>
